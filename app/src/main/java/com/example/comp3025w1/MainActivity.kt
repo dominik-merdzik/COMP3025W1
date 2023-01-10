a163a85e9edeef1b11946c327d7b2b2da8ef78d9
Dominik <dominik8878@outlook.com>
2023-01-10 17:21:43 -0500

    end of class week 1

--- app/src/main/java/com/example/comp3025w1/MainActivity.kt
+++ app/src/main/java/com/example/comp3025w1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.comp3025w1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.comp3025w1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //define a variable to associate the elements
+    // in the view to be accessible in the controller class
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // initialize the binding variable to the objects in the
+        // ActivityMainBinding class
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // set the content view to the root view of the binding object
+        setContentView(binding.root)
+
+        // create a click listener for the button
+        binding.submitButton.setOnClickListener {
+           var name = binding.nameEditText.text
+              binding.textView.text ="Hello " + name
+        }
+
+
     }
 }
